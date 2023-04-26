33b771b425646a6afaaeae2b87d85d0e69660987
Christopher G Lasater <chrisglasater@gmail.com>
2023-04-26 12:31:43 -0500

    Fixes for API proxy and fully functional api call to Serverside from Angular SIngle Page App

--- AngularCoreTemplate/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ AngularCoreTemplate/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -8,9 +8,9 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class FetchDataComponent {
     public models: SomethingModel[] = [];
 
-    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+    constructor(http: HttpClient, @Inject('BASE_URL') baseApiUrl: string) {
         //let param = new HttpParams().append('input', 'test');
-        http.get<SomethingModel[]>('https://localhost:7062/api/HelloWorld/GetSomething').subscribe(result => {
+        http.get<SomethingModel[]>(baseApiUrl + 'api/helloworld/getsomething').subscribe(result => {
             this.models = result;
         }, error => console.error(error));
     }
